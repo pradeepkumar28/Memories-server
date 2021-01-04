4eed20db6669ce8949c0cff4a5f4113f0d2d9723
pradeepkumar28 <9650381823p@gmail.com>
2021-01-04 12:57:38 +0530

    ready to deploy1

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ dotenv.config();
 app.use(bodyParser.json({ limit:"30mb", extended :true }));
 app.use(bodyParser.urlencoded({ limit:"30mb", extended :true }));
 app.use(cors());
+app.use('/posts',postRoutes);
+
 app.get('/', (res,req) =>{
     res.send("hello to memories api")
 })
-app.use('/posts',postRoutes);
-
 
 
 const PORT  = process.env.PORT || 5000 ;
