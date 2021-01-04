b5bbccd57e86fc2f80852ed60ee3ac5784b1c25e
pradeepkumar28 <9650381823p@gmail.com>
2021-01-04 13:20:26 +0530

    ready to deploy2

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ limit:"30mb", extended :true }));
 app.use(cors());
 app.use('/posts',postRoutes);
 
-app.get('/', (res,req) =>{
+app.get('/', (req,res) =>{
     res.send("hello to memories api")
 })
 
